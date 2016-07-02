a64d95e35f43784c30c4a9d46ec42c1113a66b2a
CesarAsturias <supergonsi@gmail.com>
2016-07-02 14:14:46 +0200

    Compiled for the first time

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@
 # Target: name of the output file
 TARGET = main
 # MCU: part number to build for
-MCU = TM4C123GHSPM
+MCU = TM4C123GH6PM
 # SOURCES: list of input source sources
 SOURCES = main.c startup_gcc.c
 # INCLUDES: List of includes, by default, use includes directory
@@ -18,14 +18,14 @@ INCLUDES = -Iincludes
 # OUTDIR: Directory to use for output
 OUTDIR = build
 # TIVAWARE_PATH: Path to the tivaware folder
-TIVAWARE_PATH = /home/cesar/Documentos/Tivaware
+TIVAWARE_PATH = /home/cesar/Documentos/TivaWare
 
 # LD_SCRIPT: Linker script
 LD_SCRIPT = $(MCU).ld
 
 # Define flags
 CFLAGS = -g -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp
-FLAGS +=-Os -ffunction-sections -fdata-sections -MD -std=c99 -Wall
+CFLAGS +=-Os -ffunction-sections -fdata-sections -MD -std=c99 -Wall
 CFLAGS += -pedantic -DPART_$(MCU) -c -I$(TIVAWARE_PATH)
 CFLAGS += -DTARGET_IS_BLIZZARD_RA1
 LDFLAGS = -T $(LD_SCRIPT) --entry ResetISR --gc-sections
@@ -37,7 +37,7 @@ LDFLAGS = -T $(LD_SCRIPT) --entry ResetISR --gc-sections
 # #############################
 # binaries
 # ###########################
-C = arm-none-eabi-gcc
+CC = arm-none-eabi-gcc
 LD = arm-none-eabi-ld
 OBJCOPY = arm-none-eabi-objcopy
 RM      = rm -f
@@ -54,7 +54,7 @@ $(OUTDIR)/%.o: src/%.c | $(OUTDIR)
 	$(CC) -o $@ $^ $(CFLAGS)
 
 $(OUTDIR)/a.out: $(OBJECTS)
-	$(LD) -o $@ $^ $(FLAGS)
+	$(LD) -o $@ $^ $(LDFLAGS)
 
 $(OUTDIR)/main.bin: $(OUTDIR)/a.out
 	$(OBJCOPY) -O binary $< $@
--- src/main.c
+++ src/main.c
@@ -10,19 +10,38 @@
 #include "driverlib/rom.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/pin_map.h"
 
 #define LED_BLUE GPIO_PIN_2
 
 unsigned long In; // Input from PF4
-unsigned long Out; // Output (PF2), blue LED
+unsigned long mask=4; // Mask
 
 int main(void)
 {
-	// Init PORTF
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+	// Device configuration
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN); // Configure the clocking of the device: System clock divider = 4, 
+											      // PLL is used, crystal of 16 MHz, main clock is the source clock									      
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable PORT F
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_BLUE) // PF2 as output (blue led)
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_BLUE); // PF2 as output (blue led)
+	ROM_GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0); // PF0 as input (switch 2)
+
+	// Loop
+	while(1)
+	{
+		In = ROM_GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0); // Read PORTF 0
+		In = In>>2; // Shift into position PF2
+		if (In & mask){
+			ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 1); // Set PORTF 2
+		}
+		else {
+			ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); // Reset PORTF 2
+		}
+		
+
+	}
 }
-
-
-
