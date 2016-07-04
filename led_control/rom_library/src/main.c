c37cb59adb1f0c757989e9b0607e019e9a837641
CesarAsturias <supergonsi@gmail.com>
2016-07-04 17:58:52 +0200

    Cleaning main file

--- led_control/rom_library/src/main.c
+++ led_control/rom_library/src/main.c
@@ -21,23 +21,27 @@
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
 
-uint8_t In; // Input from PF4
-unsigned long mask=4; // Mask
+uint8_t In; // Input from PF0
 
 int main(void)
 {
-	// Device configuration
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN); // Configure the clocking of the device: System clock divider = 4, 
-											      // PLL is used, crystal of 16 MHz, main clock is the source clock									      
+	// 
+	// Device configuration: PLL is used, crystal of 16 MHz, main clock is the source clock
+	// System clock divider = 4
+	//
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);      
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable PORT F
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN); // PF2 as output (blue led)
+	
+	//
+	// PF2 as output (blue led)
+	// 
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
 	ButtonsInit(); // Init buttons
 
 	// Loop
 	while(1)
 	{
 		In = ROM_GPIOPinRead(BUTTONS_GPIO_BASE, RIGHT_BUTTON); // Read PORTF 0
-		//In = In>>2; // Shift into position PF2
 		if (In==0){
 			ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_BLUE, LED_BLUE); // Turn on LED BLUE
 			ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED, 0);
